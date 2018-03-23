06491b2e1a310310fea027817de31ebebc56accd
Irvin9 <irvin_21_95@hotmail.com>
2018-03-22 21:00:08 -0600

    New: Migracion de pagos

---
+++ database/migrations/2018_03_23_014829_create_pays_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePaysTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pays', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('Articulo');
+            $table->unsignedInteger('id_usuario');
+            $table->unsignedInteger('id_articulo');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pays');
+    }
+}
