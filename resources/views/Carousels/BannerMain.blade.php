cd200b91bdf96ce4868009b491e09820d5abe19d
irvindev9 <37251305+irvindev9@users.noreply.github.com>
2018-05-13 15:40:01 -0600

    Mod: Cambio de iconos a color negro

--- resources/views/Carousels/BannerMain.blade.php
+++ resources/views/Carousels/BannerMain.blade.php
@@ -30,12 +30,12 @@
             </div>
             <div class="carousel-item">
                 <div class="row">
-                    <div class="col-sm  d-flex align-items-center"><a href="http://www.oldnavy.com/" target="_blank"><img style="max-height:100px; width:auto;" class="d-block w-100" src="{{asset('public/tiendas/oldnavy.png')}}" alt="1 slide"></div>
-                    <div class="col-sm  d-flex align-items-center"><a href="http://www.nflshop.com/" target="_blank"><img style="max-height:100px; width:auto;" class="d-block w-100" src="{{asset('public/tiendas/nfl.png')}}" alt="2 slide"></div>
-                    <div class="col-sm  d-flex align-items-center"><a href="https://www.joybuy.com/" target="_blank"><img style="max-height:100px; width:auto;" class="d-block w-100" src="{{asset('public/tiendas/joybuy.jpg')}}" alt="3 slide"></div>
-                    <div class="col-sm  d-flex align-items-center"><a href="https://store.playstation.com/es-mx/home/games" target="_blank"><img style="max-height:100px; width:auto;" class="d-block w-100" src="{{asset('public/tiendas/6.png')}}" alt="1 slide"></div>
-                    <div class="col-sm  d-flex align-items-center"><a href="https://www.walmart.com/?country=US#_ga=1.199248170.508435700.1469195625" target="_blank"><img style="max-height:100px; width:auto;" class="d-block w-100" src="{{asset('public/tiendas/walmart.png')}}" alt="2 slide"></div>
-                    <div class="col-sm  d-flex align-items-center"><a href="https://www.underarmour.com/en-us/" target="_blank"><img style="max-height:100px; width:auto;" class="d-block w-100" src="{{asset('public/tiendas/under.png')}}" alt="3 slide"></div>
+                    <div class="col-sm  d-flex align-items-center"><a href="http://www.oldnavy.com/" target="_blank"><img style="max-height:100px; width:auto;" class="d-block w-100" src="{{asset('public/tiendas/oldnavy.png')}}" alt="1 slide"></a></div>
+                    <div class="col-sm  d-flex align-items-center"><a href="http://www.nflshop.com/" target="_blank"><img style="max-height:100px; width:auto;" class="d-block w-100" src="{{asset('public/tiendas/nfl.png')}}" alt="2 slide"></a></div>
+                    <div class="col-sm  d-flex align-items-center"><a href="https://www.joybuy.com/" target="_blank"><img style="max-height:100px; width:auto;" class="d-block w-100" src="{{asset('public/tiendas/joybuy.jpg')}}" alt="3 slide"></a></div>
+                    <div class="col-sm  d-flex align-items-center"><a href="https://store.playstation.com/es-mx/home/games" target="_blank"><img style="max-height:100px; width:auto;" class="d-block w-100" src="{{asset('public/tiendas/6.png')}}" alt="1 slide"></a></div>
+                    <div class="col-sm  d-flex align-items-center"><a href="https://www.walmart.com/?country=US#_ga=1.199248170.508435700.1469195625" target="_blank"><img style="max-height:100px; width:auto;" class="d-block w-100" src="{{asset('public/tiendas/walmart.png')}}" alt="2 slide"></a></div>
+                    <div class="col-sm  d-flex align-items-center"><a href="https://www.underarmour.com/en-us/" target="_blank"><img style="max-height:100px; width:auto;" class="d-block w-100" src="{{asset('public/tiendas/under.png')}}" alt="3 slide"></a></div>
                 </div>
             </div>
         </div>
